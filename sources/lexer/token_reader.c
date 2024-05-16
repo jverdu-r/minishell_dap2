db4cb0ce8336c3c30c44edd4abe1cdf14000e5a9
Jorge Verdu ruiz <jverdu-r@c1r10s3.42madrid.com>
2024-05-16 20:04:13 +0200

    lexer bug fixed

--- sources/lexer/token_reader.c
+++ sources/lexer/token_reader.c
@@ -6,7 +6,7 @@
 /*   By: jverdu-r <jverdu-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 18:46:24 by jverdu-r          #+#    #+#             */
-/*   Updated: 2024/05/16 19:23:45 by jverdu-r         ###   ########.fr       */
+/*   Updated: 2024/05/16 20:03:02 by jverdu-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,83 +14,65 @@
 
 int	is_white_space(char c)
 {
-	if (c == '\t' || c == '\n' || c == '\v' || \
-		c == '\f' || c == '\r' || c == ' ')
+	if (c == '\t' || c == '\n' || c == '\v' ||
+			c == '\f' || c == '\r' || c == ' ')
 		return (1);
 	return (0);
 }
 
-int	get_quoted(char *args, t_lexer **list, int cr, int *ct)
+int	read_words(char *args, int i, t_lexer **list)
 {
-	char	cqt;
+	int	j;
+	int	qt;
 
-	cqt = args[cr];
-	while (args[ct[2] + ct[0]])
+	j = 0;
+	(void)list;
+	while (args[i + j])
 	{
-		if (ct[1] == 0 && (args[ct[2] + ct[0]] == '\"' \
-			|| args[ct[2] + ct[0]] == '\''))
+		if (args[i + j] == '\'' || args[i + j] == '\"')
 		{
-			cqt = args[ct[2] + ct[0]];
-			ct[1] = 1;
+			qt = i + j;
+			j++;
+			while (args[i + j] != args[qt])
+				j++;
 		}
-		else if (args[ct[2] + ct[0]] == cqt)
-			ct[1] = 0;
-		if (ct[1] == 0)
-			break ;
-		ct[0]++;
-	}
-	if (args[ct[2] + ct[0]] == ' ')
-		ct[0]--;
-	lexer_addback(list, \
-		lexer_new(ft_substr(args, cr, ct[0] + 1), 0));
-	cr += ct[0] + 1;
-	free(ct);
-	return (cr);
-}
-
-int	get_unquoted(char *args, t_lexer **list, int cr)
-{
-	int	ct;
-
-	ct = 0;
-	while (args[cr + ct] && !is_white_space(args[cr + ct]) && \
-		!check_token(args, cr + ct))
-		ct++;
-	lexer_addback(list, \
-		lexer_new(ft_substr(args, cr, ct), 0));
-	cr += ct;
-	return (cr);
-}
-
-void	read_words(char *args, t_lexer **list)
-{
-	int	cr;
-
-	cr = 0;
-	while (args[cr])
-	{
-		if (args[cr] == '\"' || args[cr] == '\'')
-			cr = get_quoted(args, list, cr, init_ct(cr));
-		else if (check_token(args, cr))
-		{
-			lexer_addback(list, \
-				lexer_new(NULL, check_token(args, cr)));
-			if (check_token(args, cr) == GREAT_GREAT || \
-					check_token(args, cr) == LESS_LESS)
-				cr = cr + 2;
-			else
-				cr++;
-		}
-		else if (is_white_space(args[cr]))
-			cr++;
+		if (is_white_space(args[i + j]) || check_token(args, i + j))
+			break;
 		else
-			cr = get_unquoted(args, list, cr);
+			j++;
 	}
+	lexer_addback(list, lexer_new(ft_substr(args, i, j), 0));
+	if (!list)
+		return (-1);
+	return (j);
 }
 
 int	token_reader(t_toolbox *tools)
 {
-	read_words(tools->args, &tools->lexer_list);
+	t_token	tk;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (tools->args[i])
+	{
+		j = 0;
+		if (is_white_space(tools->args[i]))
+			i++;
+		if (check_token(tools->args, i))
+		{
+			tk = token_handler(tools, i);
+			if (tk == LESS_LESS || tk == GREAT_GREAT)
+				j += 2;
+			else
+				j++;
+		}
+		else
+			j += read_words(tools->args, i, &tools->lexer_list);
+		i += j;
+	}
 	token_expander(tools);
 	return (0);
 }
+
+
