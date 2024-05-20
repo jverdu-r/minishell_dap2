4ca6ebfec8cfc3bcb7d67e302c187d042745f46b
Jorge Verdu ruiz <jverdu-r@c1r9s1.42madrid.com>
2024-05-20 17:52:18 +0200

    shell level added

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: jverdu-r <jverdu-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 10:28:15 by jverdu-r          #+#    #+#             */
-/*   Updated: 2024/05/15 18:03:48 by jverdu-r         ###   ########.fr       */
+/*   Updated: 2024/05/20 17:50:40 by jverdu-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,32 @@ int	g_exit_status = 0;
 void	leaks(void)
 {
 	system("leaks minishell");
+}
+
+void	shell_up(char **env)
+{
+	int		i;
+	int		lvl;
+	char	**aux;
+	char	*shlvl;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strnstr(env[i], "SHLVL", 5))
+		{
+			aux = ft_split(env[i], '=');
+			lvl = ft_atoi(aux[1]);
+			lvl++;
+			free(aux[1]);
+			aux[1] = ft_itoa(lvl);
+			shlvl = ft_strjoin("SHLVL=", aux[1]);
+			free(env[i]);
+			env[i] = shlvl;
+			free_arr(aux);
+		}
+		i++;
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -33,7 +59,10 @@ int	main(int argc, char **argv, char **envp)
 	if (envp[0] == 0)
 		tools->env = new_env();
 	else
+	{
 		tools->env = envp_dup(envp, tools);
+		shell_up(tools->env);
+	}
 	pwd_search(tools);
 	minishell_loop(tools);
 	free(tools);
